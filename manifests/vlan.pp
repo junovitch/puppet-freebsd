f2ec30f2f25f64538987323e49eea59c12b3c66d
Zach Leslie <zach@puppetlabs.com>
2012-11-16 12:12:14 -0800

    add v6address support for the vlans

--- manifests/vlan.pp
+++ manifests/vlan.pp
@@ -1,5 +1,7 @@
 define freebsd::vlan (
-  $address,  # CIDR Notation
+  $address   = '',  # CIDR Notation
+  $v6address = '',  # Address only
+  $prefixlen = '',  # Prefix length for v6 address
   $vlan,
   $dev,
   $ensure = 'present'
@@ -10,11 +12,31 @@ define freebsd::vlan (
   # We should take the correct action
   if ($ensure == 'present') {
 
+    # FIXME: when createing the vlan interface, the vlan paramaters can only be
+    # specified once, so here we are determining of we need to create a v6 and
+    # a v4 address, but then we can only set the vlandev on one of the
+    # interfaces.  So this does not allow us to use v6 only at the moment, but
+    # rather, if you want v6, then you also need to specify a v4 address.
+    # There is likely simple fix for this.
+
     # Add the configuration to rc.conf
-    shell_config { "subnet_vlan_${name}_${ifname}":
-      file  => '/etc/rc.conf',
-      key   => "ifconfig_${ifname}",
-      value => "inet ${address} vlan ${vlan} vlandev ${dev}",
+    if $address != '' {
+      shell_config { "subnet_vlan_${name}_${ifname}":
+        file  => '/etc/rc.conf',
+        key   => "ifconfig_${ifname}",
+        value => "inet ${address} vlan ${vlan} vlandev ${dev}",
+      }
+    }
+
+    if $v6address != '' {
+      if $prefixlen == '' {
+        fail('when setting a v6address on an interface, you must also specify the prefixlen')
+      }
+      shell_config { "subnet_vlan_${name}_${ifname}_ipv6":
+        file  => '/etc/rc.conf',
+        key   => "ifconfig_${ifname}_ipv6",
+        value => "inet6 ${address} prefixlen ${prefixlen}",
+      }
     }
 
     # Create the vlan interface
