869bec6813b725c2e4e90cfe546ffd04feeb97ea
Zach Leslie <zach@puppetlabs.com>
2013-04-30 13:10:35 -0700

    replace old comments

--- tests/kernel.pp
+++ tests/kernel.pp
@@ -1,4 +1,4 @@
-
+# Creates a firewall kernel configuration
 freebsd::kernel {
   "firewall":
     options => [
@@ -18,33 +18,3 @@ freebsd::kernel {
       'pfsync',
     ]
 }
-
-#file_line { 'set option ipsec':
-#  path    => '/root/kernels/FIREWALL',
-#  line    => 'options IPSEC',
-#  require => Exec["copy generic kernel to firewall"],
-#}
-
-#file_line { 'set device crypto':
-#  path    => '/root/kernels/FIREWALL',
-#  line    => 'device crypto  # needed to compile IPSEC support.',
-#  require => Exec["copy generic kernel to firewall"],
-#}
-#
-#file_line { 'set device pf':
-#  path    => '/root/kernels/FIREWALL',
-#  line    => 'device pf',
-#  require => Exec["copy generic kernel to firewall"],
-#}
-#
-#file_line { 'set device pflog':
-#  path    => '/root/kernels/FIREWALL',
-#  line    => 'device pflog',
-#  require => Exec["copy generic kernel to firewall"],
-#}
-#
-#file_line { 'set device pfsync':
-#  path    => '/root/kernels/FIREWALL',
-#  line    => 'device pfsync',
-#  require => Exec["copy generic kernel to firewall"],
-#}
